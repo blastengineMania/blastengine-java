091a754c0d94ebfcdea82d47d11f11d95d0ea581
Atsushi <atsushi@moongift.jp>
2022-03-24 10:19:40 +0900

    Build by Java11

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ rm *.jar
 rm lib/build/libs/*.jar
 gradle shadowJar
 cp lib/build/libs/*.jar .
-cp *.jar ../sample/app/libs/blastengine.jar
+mv *.jar blastengine.jar
+cp blastengine.jar ../sample/app/libs/blastengine.jar
